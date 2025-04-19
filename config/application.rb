69ce57b0c20a21085261a72919c41a574ebb3b7e
Engineer-Zahid <engineer.zahid.13@gmail.com>
2025-04-20 01:59:06 +0500

    integarte devise gem

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require 'devise/orm/active_record'
 module BookingSystem
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
